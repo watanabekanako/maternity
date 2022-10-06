d862b0518123c7b18617b87745b86545877cf5af
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-10-06 10:11:31 +0900

    baby.js追記、firebaseインストールしました。

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/items',
+        destination: 'http://localhost:8000/users',
+      },
+      {
+        source: '/api/items/:slug',
+        destination: 'http://localhost:8000/users/:slug',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/baby.js
+++ pages/baby.js
@@ -3,23 +3,32 @@ import CssBaseline from '@mui/material/CssBaseline';
 // import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import moment from 'moment';
-import { GetStaticProps } from 'next';
-const url = '';
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 const Baby = () => {
   // 今日の日付と出産予定日の差を出力したい
   // 出産予定日はfetchにて持ってくる
+  const { data, error } = useSWR(
+    'http://localhost:8000/user',
+    fetcher
+  );
 
   const moment = require('moment');
   const m1 = moment('2022/12/20');
   const m2 = moment(moment().format('YYYY-MM-DD'));
   const diff = m1.diff(m2, 'days'); // 396
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <p>出産予定日まであと{diff}日</p>
-      </Container>
-    </React.Fragment>
-  );
+  data.map((user) => {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <p>出産予定日まであと{diff}日</p>
+          <div>{user.birthDate}</div>
+        </Container>
+      </React.Fragment>
+    );
+  });
 };
 export default Baby;
