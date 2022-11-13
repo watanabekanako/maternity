a7cd8818fbae9fca0b28a8018f352ffd2187d3a5
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-11-13 11:24:58 +0900

    login画面編集しました。

--- pages/login/index.js
+++ pages/login/index.js
@@ -11,7 +11,10 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
-
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import { ReviewsSharp } from '@mui/icons-material';
 const Login = () => {
   const router = useRouter();
   const [formValues, setFormValues] = React.useState({});
@@ -33,20 +36,9 @@ const Login = () => {
       formValues.password
     );
   };
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
+  // const handleChange = (prop) => (event) => {
+  //   setValues({ ...values, [prop]: event.target.value });
+  // };
 
   React.useEffect(() => {
     // ユーザーがある = ログインできた時の処理
@@ -54,7 +46,19 @@ const Login = () => {
       router.push('/baby');
     }
   }, [user]);
+
+  // パスワードを表示するかどうか
   const [isRevealPassword, setIsRevealPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    // 目隠しボタンをクリックした時の処理
+    // set関数の引数には更新したい状態を渡す
+    setIsRevealPassword(!isRevealPassword);
+  };
+  const handleMouseDownPassword = (event) => {
+    //マウスダウンしたときの挙動制御
+    event.preventDefault();
+  };
   return (
     <DefaultLayout>
       <Box textAlign="center">
@@ -76,37 +80,41 @@ const Login = () => {
           />
         </div>
         <div>
-          <TextField
-            id="outlined-basic"
-            label="パスワード"
-            variant="outlined"
-            margin="dense"
-            sx={{ width: 600, margin: 10 }}
-            // type="passwordType"
-            type={isRevealPassword ? 'text' : 'password'}
-            value={formValues.password}
-            onChange={(e) => {
-              setFormValues({
-                ...formValues,
-                password: e.target.value,
-              });
-            }}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {formValues.showPassword ? (
-                    <VisibilityOff />
-                  ) : (
-                    <Visibility />
-                  )}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
+          <FormControl sx={{ width: 600, margin: 10 }}>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              label="パスワード"
+              margin="dense"
+              // 見せる時はテキスト、見せないときはパスワード
+              type={isRevealPassword ? 'text' : 'password'}
+              value={formValues.password}
+              onChange={(e) => {
+                setFormValues({
+                  ...formValues,
+                  password: e.target.value,
+                });
+              }}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {formValues.showPassword ? (
+                      <VisibilityOffIcon />
+                    ) : (
+                      <VisibilityIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
         </div>
         <Button variant="contained" onClick={handleLogin}>
           ログイン
