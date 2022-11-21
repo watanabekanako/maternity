dfe2a9763421e59ee2eddabaf655444d8cfbfb23
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-11-21 14:57:05 +0900

    diaryCalender編集中

--- pages/diaryCalender/index.jsx
+++ pages/diaryCalender/index.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import DefaultLayout from '../../components/layout/DefaultLayout';
 import 'react-calendar/dist/Calendar.css';
+import DiaryCalenderModal from '../../components/layout/DiaryCalenderModal';
 const MyApp = () => {
+  const [open, setOpen] = React.useState(false);
+  // モーダル
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   const [value, onChange, event] = useState(new Date());
   console.log(value);
   return (
@@ -18,6 +27,7 @@ const MyApp = () => {
             // }}
           />
         </div>
+        <DiaryCalenderModal />
       </DefaultLayout>
     </>
   );
