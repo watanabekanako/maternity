77a6f41606085c8477ee84b9aaa17c9f495e9f88
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-11-27 16:20:47 +0900

    ダイアログの表示完了

--- pages/diaryCalender/index.jsx
+++ pages/diaryCalender/index.jsx
@@ -4,45 +4,73 @@ import DefaultLayout from '../../components/layout/DefaultLayout';
 import 'react-calendar/dist/Calendar.css';
 import DiaryCalenderModal from '../../components/layout/DiaryCalenderModal';
 import Modal from '@mui/material/Modal';
+import { Box } from '@mui/system';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 const MyApp = () => {
-  const [selectedDate, setSelectedDate] = React.useState();
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
 
-  const [value, onChange, event] = useState(new Date());
+  // useStateになにもわたさないとundefind(=未定義)が初期値になる
+  const [selectedValue, setSelectedValue] = useState();
+
   // console.log(value);
-  //valueでクリックした日付を取ることができる
-  const [today, setToday] = useState();
+  // const handleClose = () => onClose(selectedValue);
+  console.log(selectedValue);
 
+  // ダイアログ開閉
+  const handleClose = () => {
+    setSelectedValue(undefined);
+  };
   return (
     <>
       <DefaultLayout>
         <div>
           <h1>カレンダー</h1>
           <Calendar
-            onChange={onChange}
-            value={today}
+            // onChange={onChange}
+            value={selectedValue}
             onClickDay={(value, event) => {
-              event.target.value;
-              // event,target.valueにてクリックした日付を取得
-              console.log(event.target.value);
+              setSelectedValue(value);
             }}
           />
-          <Modal
+          {/* <Modal
             open={selectedDate}
             // このopenにonClickDay渡す?
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
-          ></Modal>
+          >
+            <Box></Box>
+          </Modal> */}
+
+          <Dialog onClose={handleClose} open={selectedValue}>
+            <DialogTitle>日付</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="text"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>編集</Button>
+              <Button onClick={handleClose}>保存</Button>
+            </DialogActions>
+          </Dialog>
         </div>
         <DiaryCalenderModal />
       </DefaultLayout>
     </>
   );
 };
-
-// カレンダー
-// onClickDayを押したらモーダルが開くようにしたい
 
 export default MyApp;
